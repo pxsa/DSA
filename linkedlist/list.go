dfa40af6ab65236ae890dd9d6b395855af078f6c
pxsa <paghsis@gmail.com>
2023-07-30 17:52:57 +0330

    write test and other function

--- linkedlist/list.go
+++ linkedlist/list.go
@@ -17,7 +17,7 @@ type LinkedList struct {
 func (l *LinkedList) Append(data any) {
 	
 	new := &Node{Data: data, Next: nil}
-	
+
 	if l.Head == nil {
 		l.Head = new
 		return
@@ -32,15 +32,90 @@ func (l *LinkedList) Append(data any) {
 	cur.Next = new
 }
 
-// Print prints all nodes of linkedlist
-func (l *LinkedList) Print() {
+// Prepend prepends a new node to the linkedlist
+func (l *LinkedList) Prepend(data any) {
+
+	new_node := &Node{
+		Data: data,
+		Next: l.Head,
+	}
+
+	l.Head = new_node
+}
+
+// Insert inserts a new node at a specific location
+func (l *LinkedList) Insert(data any, index int) {
+	if index == 0 {
+		l.Prepend(data)
+	} else if (index == l.Len()-1) {
+		l.Append(data)
+	} else if index < l.Len() {
+		
+		var curIndex int
+		preNode := l.Head
+
+		for curIndex != index-1 {
+			curIndex++
+			preNode = preNode.Next
+		}
+
+		newNode := &Node{Data: data, Next: preNode.Next}
+		preNode.Next = newNode
+
+	}
+}
+
+// RemoveFirst removes the first node from list
+func (l *LinkedList) RemoveFirst() error {
+	if l.Len() == 0 {
+		return fmt.Errorf("list is empty")
+	}
+
+	l.Head = l.Head.Next
+
+	return nil
+}
+
+// Remove removes the specified node from list
+func (l *LinkedList) Remove(index int) error {
+
+	if l.Len() == 0 {
+		return fmt.Errorf("list is empty")
+	}
+
+	if index >= l.Len() {
+		return fmt.Errorf("index out of range")
+	}  
+
 	cur := l.Head
-	var index int
-	for cur != nil {
-		index += 1
-		fmt.Printf("<Node #%d>: %v", index, cur.Data)
+	curIndex := 0
+	for curIndex != index-1 {
+		curIndex++
 		cur = cur.Next
 	}
+
+	if index == l.Len() - 1 {
+		cur.Next = nil
+	} else {
+		cur.Next = cur.Next.Next
+	}
+
+	return nil
+}
+
+// Print prints all nodes of linkedlist
+func (l *LinkedList) Print() string {
+	cur := l.Head
+	var index int
+	var res string
+
+	for cur != nil {
+		index += 1
+		res += fmt.Sprintf("<Node #%d>: %v\n", index, cur.Data)
+		cur = cur.Next
+	}
+
+	return res
 }
 
 // Len returns the length of the linkedlist
@@ -54,4 +129,24 @@ func (l *LinkedList) Len() int {
 	}
 
 	return count
+}
+
+func (l *LinkedList) GetNode(index int) (*Node, error) {
+	curIndex := 0
+	cur := l.Head
+
+	if index >= l.Len() {
+		return nil, fmt.Errorf("out of range")
+	}
+
+	if l.Len() == 0 {
+		return nil, fmt.Errorf("list is empty")
+	}
+
+	for curIndex < index {
+		cur = cur.Next
+		curIndex += 1
+	}
+
+	return cur, nil
 }
--- linkedlist/list_test.go
+++ linkedlist/list_test.go
@@ -18,3 +18,111 @@ func TestAppend(t *testing.T) {
 		t.Errorf("expected to recieve %d but but recieve %d", 2, l.Len())
 	}
 }
+
+func TestPrint(t *testing.T) {
+	l := &LinkedList{}
+
+	l.Append("one")
+	l.Append("two")
+
+	res := l.Print()
+	expected_answer := "<Node #1>: one\n<Node #2>: two\n"
+
+	if res != expected_answer {
+		t.Errorf("expected %s but recieved %s", expected_answer, res)
+	}
+	
+}
+
+func TestPrepend(t *testing.T) {
+	l := &LinkedList{}
+
+	l.Prepend("one")
+
+	if l.Len() != 1 {
+		t.Errorf("expected %v but got %v instead.", 1, l.Len())
+	}
+
+	l.Prepend("two")
+	l.Prepend("three")
+
+	if l.Len() != 3 {
+		t.Errorf("expected %v but got %v instead.", 3, l.Len())
+	}
+
+	res, err := l.GetNode(1)
+	if err != nil {
+		t.Error(err)
+	} else if res.Data != "two" {
+		t.Errorf("expected %v but got %v instead.", "two", res.Data)
+	}
+
+	res, err = l.GetNode(2)
+	if err != nil {
+		t.Error(err)
+	} else if res.Data != "one" {
+		t.Errorf("expected %v but got %v instead.", "one", res.Data)
+	}
+
+	res, err = l.GetNode(2)
+	if err != nil {
+		t.Error(err)
+	} else if res.Data != "one" {
+		t.Errorf("expected %v but got %v instead.", "one", res.Data)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	l := &LinkedList{}
+
+	err := l.Remove(2)
+	if err == nil {
+		t.Error("expected an error but didn't recieve any (list is empty)")
+	}
+
+	l.Append("one")
+	l.Prepend("two")
+	l.Append("three")
+
+	err = l.Remove(1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if l.Len() != 2 {
+		t.Errorf("expected %v but got %v instead.", 2, l.Len())
+	}
+
+	res, err := l.GetNode(1)
+	if err != nil {
+		t.Error(err)
+	} else if res.Data != "three" {
+		t.Errorf("expected %v but got %v instead.", "three", res.Data)
+	}
+
+	err = l.Remove(12)
+	if err == nil {
+		t.Error("expected an error but didn't recieve any (index out of range)")
+	}
+}
+
+func TestInse(t *testing.T) {
+	l := &LinkedList{}
+
+	l.Insert("zero", 2)
+	l.Insert("one", 0)
+	l.Insert("two", 0)
+	l.Insert("three", 1)
+	l.Insert("four", 1)
+
+	if l.Len() != 4 {
+		t.Errorf("expected %v but got %v instead.", 4, l.Len())
+	}
+
+	res, err := l.GetNode(1)
+	if err != nil {
+		t.Error(err)
+	} else if res.Data != "four" {
+		t.Errorf("expected %v but got %v instead.", "four", res.Data)
+	}
+}
